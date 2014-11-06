fb1cf680ccd2963e54f5beea95f5106cf97915c6
ssut <ssut@ssut.me>
2014-11-06 20:25:53 +0900

    release 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='basehangul',
-    version='1.0',
+    version='1.1',
     description='Human-readable binary encoding, BaseHangul for Python',
     author='SuHun Han',
     author_email='ssut@ssut.me',
